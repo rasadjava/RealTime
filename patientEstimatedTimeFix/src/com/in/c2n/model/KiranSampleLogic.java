0f1eb1fded2606bf95fae9dfdcd04e3da17cca2f
Prasad <rasadjava@gmail.com>
2018-03-09 12:01:41 +0530

    just added adition logic to my samplekiran

---
+++ patientEstimatedTimeFix/src/com/in/c2n/model/KiranSampleLogic.java
@@ -0,0 +1,15 @@
+package com.in.c2n.model;
+
+public class KiranSampleLogic {
+
+	public static void main(String[] args) {
+		KiranSampleLogic.sampleKiran();
+	}
+	public static  void sampleKiran() {
+		int a=10;
+		int b=20;
+		int c = a+b;
+		System.out.println(c);
+}		
+
+}
