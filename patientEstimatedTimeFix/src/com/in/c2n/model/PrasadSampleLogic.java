31e47b23804b6529320350ed985e8dc363639412
PRASAD BABU NAIDU <31297589+rasadjava@users.noreply.github.com>
2018-03-09 11:40:54 +0530

    Prasad Commited new class-08032018-1140

---
+++ patientEstimatedTimeFix/src/com/in/c2n/model/PrasadSampleLogic.java
@@ -0,0 +1,14 @@
+package com.in.c2n.model;
+
+public class PrasadSampleLogic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PrasadSampleLogic.prasadMethod();
+
+	}
+
+	public static void prasadMethod() {
+		System.out.println("09032018");
+	}
+}
